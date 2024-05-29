1e4296a1b7fa5f45b68a896a7afc2f968108c9a3
eomjinyoung <jinyoung.eom@gmail.com>
2024-05-29 10:58:36 +0900

    .

--- Hello.java
+++ Hello.java
@@ -1,11 +1,11 @@
 public class Hello {
-  public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        int a = 100;
 
-    int a = 100;
-
-    if (a < 100) {
-      System.out.println("하하하");
+        // 테스트입니다.
+        if (a < 100) {
+            System.out.println("하하하");
+        }
     }
-  }
 }
